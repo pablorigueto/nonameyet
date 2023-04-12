1eb65cb84fa0f62b5a699648b4b861dc898d570c
rigueto <pabloedurigueto@outlook.com>
2023-04-12 20:24:15 -0300

    Add owl carousel lib, comments on node and template twig to node.

---
+++ web/themes/custom/yes_pet_app/js/custom/carousel.js
@@ -0,0 +1,29 @@
+(function ($, Drupal) {
+
+	Drupal.behaviors.carousels = {
+    attach() {
+      if (!drupalSettings.carousels) {
+        $(document).ready(function () {
+					$('.owl-carousel').owlCarousel({
+						autoplay: false,
+						autoplayTimeout: 5000,
+						autoplayHoverPause: true,
+						thumbs: true,
+						merge:true,
+						responsive: {
+							0:{
+								items:1,
+								nav:true,
+								loop:true,
+								dots: true,
+								center: true,
+							}
+						},
+					});
+        });
+        drupalSettings.carousels = true;
+      }
+    }
+  }
+
+})(jQuery, Drupal);
