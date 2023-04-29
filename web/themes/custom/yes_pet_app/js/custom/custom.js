d4371b24fc0846f6efbbf3f20473182b42210545
rigueto <pabloedurigueto@outlook.com>
2023-04-29 12:05:31 -0300

    Adjustments on reply, edit, del comment screen.

---
+++ web/themes/custom/yes_pet_app/js/custom/custom.js
@@ -0,0 +1,49 @@
+(function ($, Drupal) {
+
+  // It's to core comment bug on edit/del that was edit
+  // and didn't render the last revision of comment. 
+  Drupal.behaviors.moveDivToInsideEditDel = {
+    attach() {
+      if (!drupalSettings.moveDivToInsideEditDel) {
+        const commentEditDiv = document.querySelector('.comment_edit')
+        // Check first if the dom has this class.
+        if (commentEditDiv) {
+          const ol = document.querySelector('.breadcrumb__list');
+          const li = ol.children[1];
+          ol.replaceChild(commentEditDiv, li);
+          commentEditDiv.removeAttribute('style');
+        }
+        // Set the flag to indicate that the behavior has been executed for this page.
+        drupalSettings.moveDivToInsideEditDel = true;
+      }
+    }
+  };
+
+  // It's to avoid count when clicks on score of vote/up/down.
+  Drupal.behaviors.avoidDefaultCountVotes = {
+    attach() {
+      if (!drupalSettings.avoidDefaultCountVotes) {
+
+        const voteScoreElements = document.querySelectorAll('.vote-current-score');
+        voteScoreElements.forEach(function(element) {
+          element.addEventListener('click', function(event) {
+            event.preventDefault();
+            // console.log('test');
+          });
+        });
+
+        // Set the flag to indicate that the behavior has been executed for this page.
+        drupalSettings.avoidDefaultCountVotes = true;
+      }
+    }
+  };
+
+  // Returns current URL.
+  function currentUrl() {
+    return $(location).attr('href');
+  }
+
+})(jQuery, Drupal);
+
+
+ 
