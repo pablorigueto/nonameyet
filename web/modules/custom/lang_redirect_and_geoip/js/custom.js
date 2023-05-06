6945b71e72677766f046118ab70476d46b4ee596
rigueto <pabloedurigueto@outlook.com>
2023-05-06 11:44:22 -0300

    Add geoip system and new module.

---
+++ web/modules/custom/lang_redirect_and_geoip/js/custom.js
@@ -0,0 +1,109 @@
+(function ($, Drupal) {
+
+  // Language switch.
+  Drupal.behaviors.dropDown = {
+    attach() {
+      $(document).ready(function () {
+        if (!drupalSettings.dropDown) {
+ 
+          // Toggle the visibility of the dropdown menu when its link is clicked
+          const dropdownToggles = document.querySelectorAll('.dropdown > a');
+          dropdownToggles.forEach(toggle => {
+            toggle.addEventListener('click', function(e) {
+              e.preventDefault();
+              const dropdownMenu = this.nextElementSibling;
+              if (dropdownMenu.style.display === 'none' || !dropdownMenu.style.display) {
+                dropdownMenu.style.display = 'block';
+              } else {
+                dropdownMenu.style.display = 'none';
+              }
+            });
+          });
+
+          // Add active class to the clicked link and hide the dropdown menu
+          const links = document.querySelectorAll('.dropdown a, .dropdown img');
+          links.forEach(link => {
+            link.addEventListener('click', function(event) {
+              event.preventDefault(); // prevent the default link behavior
+              const langcode = link.getAttribute('langcode'); // get the langcode attribute
+
+              // Get the current URL
+              const currentUrl = window.location.href;
+
+              // Check if the URL contains the language code
+              if (currentUrl.includes(langcode)) {
+                return;
+              }
+
+              if (langcode == 'pt-br' || langcode == 'en-us' ) {
+                window.location.href = `/${langcode}/home`; // redirect to the language page using the langcode
+              }
+            });
+          });
+
+          // Set the flag to indicate that the behavior has been executed for this page.
+          drupalSettings.dropDown = true;
+        }
+      })
+    }
+  };
+
+
+  // Check the link of left menu.
+  Drupal.behaviors.leftMenuLink = {
+    attach() {
+      $(document).ready(function () {
+        if (!drupalSettings.leftMenuLink) {
+
+          const firstLink = document.querySelector('#block-yes-pet-app-main-menu .menu__link--level-1:first-child');
+          firstLink.addEventListener('click', function(e) {
+            e.preventDefault();
+            const hrefValue = firstLink.getAttribute('href');
+            const home = hrefValue + '/home';
+
+            // To get the path.
+            const url = new URL(window.location.href);
+            const path = url.pathname;
+
+            if (path !== home) {
+              window.location.href = home;
+            }
+
+          });
+          // Set the flag to indicate that the behavior has been executed for this page.
+          drupalSettings.leftMenuLink = true;
+        }
+      })
+    }
+  };
+
+  // Check the link of left menu.
+  Drupal.behaviors.logoRedirect = {
+    attach() {
+      $(document).ready(function () {
+        if (!drupalSettings.logoRedirect) {
+
+          const firstLink = document.querySelector('.reader__logo a');
+          firstLink.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            // To get the path.
+            const url = new URL(window.location.href);
+            const path = url.pathname.split('/');
+            let lang = '/' + path[1] + '/home';
+
+            if (!url.pathname.endsWith("/home")) {
+              window.location.href = lang;
+            }
+
+          });
+          // Set the flag to indicate that the behavior has been executed for this page.
+          drupalSettings.logoRedirect = true;
+        }
+      })
+    }
+  };
+
+
+  
+})(jQuery, Drupal);
