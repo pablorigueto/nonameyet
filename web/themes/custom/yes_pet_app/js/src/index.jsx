776ca7fcfd6ca4f3417a742dc40f51ec7ca893d0
rigueto <pabloedurigueto@outlook.com>
2023-05-11 18:15:43 -0300

    Add Dog on loading screen.

--- web/themes/custom/yes_pet_app/js/src/index.jsx
+++ web/themes/custom/yes_pet_app/js/src/index.jsx
@@ -11,11 +11,45 @@ const rootElement = document.getElementById('content-main-react');
 const Loading = () => {
   return (
     <div className="loader">
+      {/* <div className="loader"></div> */}
 
-      <div className="loader"></div>
+      <div className="dog">
+        <div className="dog-body">
+          <div className="dog-tail">
+            <div className="dog-tail">
+              <div className="dog-tail">
+                <div className="dog-tail">
+                  <div className="dog-tail">
+                    <div className="dog-tail">
+                      <div className="dog-tail">
+                        <div className="dog-tail"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="dog-torso"></div>
+        <div className="dog-head">
+          <div className="dog-ears">
+            <div className="dog-ear"></div>
+            <div className="dog-ear"></div>
+          </div>
+          <div className="dog-eyes">
+            <div className="dog-eye"></div>
+            <div className="dog-eye"></div>
+          </div>
+          <div className="dog-muzzle">
+            <div className="dog-tongue"></div>
+          </div>
+        </div>
+      </div>
+
+      <div className="ball" tabIndex="0"></div>
 
     </div>
-
 
   );
 };
